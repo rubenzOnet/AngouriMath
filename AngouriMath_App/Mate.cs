6e957c1aa07c2bd452afe535ebe645add7874158
Rubenz <rgarrido@officenet.com.mx>
2023-01-23 12:19:40 -0600

    Se eliminaron los using inecesarios

--- AngouriMath_App/Mate.cs
+++ AngouriMath_App/Mate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace AngouriMath_App
 {
